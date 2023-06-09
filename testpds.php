ab4129c799923b808681721d60398db22b037514
Rainmtr <90014448+Rainmtr@users.noreply.github.com>
2023-06-09 20:47:50 +0700

    Add files via upload

--- testpds.php
+++ testpds.php
@@ -9,8 +9,32 @@
 
     $result = $collection->find( [ 'author' => 'Brandon Sanderson'] );
 
+    // Get Array Key
     foreach ($result as $document) {
-        echo $document['title'], "</br>";
+        $keys = array_keys($document->getArrayCopy());
+        foreach ($keys as $key) {
+            echo $key, "<br/>";
+        }
+    }
+
+    // Print all attributes
+    foreach ($result as $document) {
+        get_object_value($document);
+        echo "</br>";
+    }
+
+    function get_object_value($arr) {
+        if ($arr instanceof MongoDB\Model\BSONArray || $arr instanceof MongoDB\Model\BSONDocument) {
+            $arr = json_decode(json_encode($arr), true);
+        }
+
+        foreach ($arr as $key => $val) {
+            if (is_array($val)) {
+                get_object_value($val);
+            } else {
+                echo "$key = $val <br/>";
+            }
+        }     
     }
 ?>
 
