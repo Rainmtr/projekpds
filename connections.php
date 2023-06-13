1abac147b98385d4ce2ed59d398503d243fac4ed
Rainmtr <90014448+Rainmtr@users.noreply.github.com>
2023-06-13 17:43:06 +0700

    Add files via upload

---
+++ connections.php
@@ -0,0 +1,8 @@
+<?php
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "proyek_pds";
+
+    $conn = new mysqli($server, $username, $password, $database);
+?>
