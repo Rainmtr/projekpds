18bf55a2c3c008bcad18e59d309383b7456f6da5
Rainmtr <pampangr8@gmail.com>
2023-06-14 15:35:09 +0700

    Update testpds.php

--- testpds.php
+++ testpds.php
@@ -9,32 +9,38 @@
 
     $result = $collection->find( [ 'author' => 'Brandon Sanderson'] );
 
-    // Get Array Key
-    foreach ($result as $document) {
-        $keys = array_keys($document->getArrayCopy());
-        foreach ($keys as $key) {
-            echo $key, "<br/>";
-        }
-    }
-
-    // Print all attributes
+    // Basic Print
     foreach ($result as $document) {
         get_object_value($document);
         echo "</br>";
     }
 
-    function get_object_value($arr) {
-        if ($arr instanceof MongoDB\Model\BSONArray || $arr instanceof MongoDB\Model\BSONDocument) {
-            $arr = json_decode(json_encode($arr), true);
-        }
+    // // Get Array Key
+    // foreach ($result as $document) {
+    //     $keys = array_keys($document->getArrayCopy());
+    //     foreach ($keys as $key) {
+    //         echo $key, "<br/>";
+    //     }
+    // }
 
-        foreach ($arr as $key => $val) {
-            if (is_array($val)) {
-                get_object_value($val);
-            } else {
-                echo "$key = $val <br/>";
-            }
-        }     
-    }
+    // // Print all attributes
+    // foreach ($result as $document) {
+    //     get_object_value($document);
+    //     echo "</br>";
+    // }
+
+    // function get_object_value($arr) {
+    //     if ($arr instanceof MongoDB\Model\BSONArray || $arr instanceof MongoDB\Model\BSONDocument) {
+    //         $arr = json_decode(json_encode($arr), true);
+    //     }
+
+    //     foreach ($arr as $key => $val) {
+    //         if (is_array($val)) {
+    //             get_object_value($val);
+    //         } else {
+    //             echo "$key = $val <br/>";
+    //         }
+    //     }     
+    // }
 ?>
 
