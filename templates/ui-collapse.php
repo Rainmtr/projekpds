a4cc807dad8b4c823522240749e3ba49b6397946
Rainmtr <90014448+Rainmtr@users.noreply.github.com>
2023-06-16 10:40:56 +0700

    Update ui-collapse.php

--- templates/ui-collapse.php
+++ templates/ui-collapse.php
@@ -37,6 +37,23 @@
         }
         echo "</br>";
     }
+
+        // foreach ($result as $document) {
+    //     echo $document['transaction_id'] . "</br>";
+    //     echo $document['cust_id'] . "</br>";
+    //     echo $document['date'] . "</br>";
+    //     echo $document['ship_country'] . "</br>";
+
+    //     $array_doc = json_decode(json_encode($document['items']), true);
+    //     foreach ($array_doc as $doc) {
+    //         foreach($doc as $key => $value) {
+    //             echo $key . " = " . $value . "</br>";
+    //         }
+    //     }
+
+    //     echo $document['total_price'] . "</br>";
+    //     echo "</br>";
+    // }
 ?>
 
 <!DOCTYPE html>
